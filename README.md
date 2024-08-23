f19abf96211277feb59962bbeb0676a021cfa082
Emad Dehnavi <dehnavi@gmx.com>
2024-08-23 11:51:10 +0200

    clustered_web_services

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # system-design-diagrams
 Using Diagram library, we can make cool system design charts using Python
 
-![Hello World](web_service.png)
+![clustered_web_services](clustered_web_services.png)
--- system_design/main.py
+++ system_design/main.py
@@ -1,11 +1,30 @@
 # set GROQ_API_KEY in the secrets
-from diagrams import Diagram
-from diagrams.aws.compute import EC2
-from diagrams.aws.database import RDS
-from diagrams.aws.network import ELB
+from diagrams import Cluster, Diagram
+from diagrams.aws.compute import EC2, ECS
+from diagrams.aws.database import ElastiCache, RDS
+from diagrams.aws.network import ELB, Route53
 
 
 def start():
     print("The basic starter design")
     with Diagram("Web Service", show=False):
         ELB("lb") >> EC2("web") >> RDS("userdb")
+
+
+def clusteredWebSerivce():
+    with Diagram("Clustered Web Services", show=False):
+        dns = Route53("dns")
+        lb = ELB("lb")
+
+        with Cluster("Services"):
+            svc_group = [ECS("web1"), ECS("web2"), ECS("web3")]
+
+        with Cluster("DB Cluster"):
+            db_primary = RDS("userdb")
+            db_primary - [RDS("userdb ro")]
+
+        memcached = ElastiCache("memcached")
+
+        dns >> lb >> svc_group
+        svc_group >> db_primary
+        svc_group >> memcached
